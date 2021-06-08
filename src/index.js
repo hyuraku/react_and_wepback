26aacf9a54a3132790817a133019b8949a1fda9c
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-06-08 21:08:12 +0900

    add react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
- (function helloWorld() {
-   console.log('Hello World');
- }());
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = () => {
+  return <h1>Hello World</h1>;
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
